c003d311041b83886914233550b6a583869c1cfa
RuslanEA7 <rus.baraksanov@gmail.com>
2024-10-23 20:05:51 +0700

    23.10.24

--- 23.10.24.py
+++ 23.10.24.py
@@ -1,5 +1,10 @@
 # Задание №1.
 
 # string = input("Введите 2 числа:").split(" ")
-#
+# numbers = []
 # for i in range(int(string[0]), int(string[1])):
+#     count = 0
+#     for j in range(i):
+#         if j % 2 == 0:
+#             count += 1
+#         print("Кол-во:", count, "По число:", i)
