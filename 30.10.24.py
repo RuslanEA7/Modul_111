081c63becc003d9776a90fb18596a80a7233028c
RuslanEA7 <rus.baraksanov@gmail.com>
2024-10-30 20:19:11 +0700

    30.10.24

---
+++ 30.10.24.py
@@ -0,0 +1,63 @@
+# import pandas as pd
+import re
+
+# list = [] # Список.
+
+# d = {
+#     0: "Toyota",
+#     1: "Lexus",
+#     4: "Geely"
+# }  # Словарь.
+#
+# d[4] = "65451121"
+# print(d.get(1))
+# print(d[4])
+#
+# for keys,value in d.items():
+#     # print(keys," : ", value)
+#     if keys == 1:
+#         print(keys, " : ", value)
+# e = d.copy()
+# print(e)
+# print(e.pop(4))
+# print(e)
+# e = d.get("car", 0)
+# print(e)
+
+
+# c = (1,2,3,54,6,7,8,8)
+# print(set(c))
+# f = set(c)
+# f.add(12)
+# print(f)
+
+
+# keys = (1,2,3,4,5,6,7,8)
+# # print(min(c))
+# value = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+# d1 = {}
+# for keys,value in zip(keys,value):
+#     d1[keys] = value
+# print(d1)
+
+# df = pd.DataFrame()
+#
+# print(df)
+
+
+
+
+
+
+
+
+# Регулярные выражения.
+
+# s = input("Введите логин:")
+#
+# pattern = "(user)|(admin)+"
+#
+# st = re.search(pattern, s)
+# print(st)
+# if not st:
+#     print("Логин верный:", s)   # Пишем в базу
