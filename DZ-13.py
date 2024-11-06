ca7776b75409b57406f0d798a5cc7865949715c7
RuslanEA7 <rus.baraksanov@gmail.com>
2024-11-06 20:11:51 +0700

    06.11.24

--- DZ-13.py
+++ DZ-13.py
@@ -9,34 +9,34 @@
 
 # Образец №2.
 
-import re
-s = input("Введите пример:")
-operation = re.findall(r'[\-/\+*]', s)
-print(operation)
-numbers = re.split(r'[(\-/\+*)]', s.replace(" ", ""))
-print(numbers)
-i = 0
-result = 0
-res = []
-j = 0
-isk = []
-while j < len(numbers):
-    print(operation[j])
-    if operation[j] == "/":
-        res.append(int(numbers[j]) / int(numbers[j+1]))
-        numbers.pop(j)
-        numbers.pop(j)
-        operation.pop(j)
-        j = 0
-    if operation[j] == "*":
-        res.append(int(numbers[j]) * int(numbers[j + 1]))
-        numbers.pop(j)
-        numbers.pop(j)
-        operation.pop(j)
-        j = 0
-    j += 1
-print(operation, numbers)
-print(res)
+# import re
+# s = input("Введите пример:")
+# operation = re.findall(r'[\-/\+*]', s)
+# print(operation)
+# numbers = re.split(r'[(\-/\+*)]', s.replace(" ", ""))
+# print(numbers)
+# i = 0
+# result = 0
+# res = []
+# j = 0
+# isk = []
+# while j < len(numbers):
+#     print(operation[j])
+#     if operation[j] == "/":
+#         res.append(int(numbers[j]) / int(numbers[j+1]))
+#         numbers.pop(j)
+#         numbers.pop(j)
+#         operation.pop(j)
+#         j = 0
+#     if operation[j] == "*":
+#         res.append(int(numbers[j]) * int(numbers[j + 1]))
+#         numbers.pop(j)
+#         numbers.pop(j)
+#         operation.pop(j)
+#         j = 0
+#     j += 1
+# print(operation, numbers)
+# print(res)
 
 
 
